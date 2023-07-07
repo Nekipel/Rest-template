29bbc56125dd46cf6aa6f2f1ca804bf4ff6156ae
Анатолий и Алена Дом <an@MacBook-Pro-Anatolij.local>
2023-07-07 16:54:40 +0300

    feat: complete task

--- src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
+++ src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
@@ -1,13 +1,109 @@
 package ru.kata.spring.boot_security.demo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.*;
+import ru.kata.spring.boot_security.demo.model.User;
 
 @SpringBootApplication
 public class SpringBootSecurityDemoApplication {
 
+	static RestTemplate restTemplate = new RestTemplate();
+	static String baseUrl = "http://94.198.50.185:7081/api/users";
+	private static String cookie;
+	private static String result = "";
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
+//		SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
+		useExchangeMethodOfRestTemplate();
+	}
+
+	public static void useExchangeMethodOfRestTemplate() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		HttpEntity<Object> requestEntity = new HttpEntity<>(headers);
+		getUsersByExchangeMethod(requestEntity);
+
+		User user = new User();
+		user.setId(3L);
+		user.setName("James");
+		user.setLastName("Brown");
+		user.setAge((byte) 33);
+		headers.add("Cookie", cookie);
+		HttpEntity<Object> requestEntityPost = new HttpEntity<>(user,headers);
+		addUserByExchangeMethod(requestEntityPost);
+		user.setName("Thomas");
+		user.setLastName("Shelby");
+		HttpEntity<Object> requestEntityPut = new HttpEntity<>(user,headers);
+		updateUserByExchangeMethod(requestEntityPut);
+		HttpEntity<Object> requestEntityDelete = new HttpEntity<>(headers);
+		deleteUserByExchangeMethod(requestEntityDelete);
+		System.out.println(result);
+	}
+
+	public static void getUsersByExchangeMethod(HttpEntity<Object> requestEntity){
+		ResponseEntity<String> responseEntity = restTemplate.exchange(
+				baseUrl,
+				HttpMethod.GET,
+				requestEntity,
+				String.class);
+		HttpStatus statusCode = responseEntity.getStatusCode();
+		System.out.println("Status code " + statusCode);
+
+		String users =  responseEntity.getBody();
+		System.out.println("response body " + users);
+
+		HttpHeaders responseHeaders = responseEntity.getHeaders();
+		cookie = responseHeaders.getFirst("Set-Cookie");
+		System.out.println("response Headers " + responseHeaders);
+		System.out.println("response Cookie " + cookie);
+	}
+
+	public static void addUserByExchangeMethod(HttpEntity<Object> requestEntity){
+		ResponseEntity<String> responseEntity = restTemplate.exchange(
+				baseUrl,
+				HttpMethod.POST,
+				requestEntity,
+				String.class);
+		HttpStatus statusCode = responseEntity.getStatusCode();
+		System.out.println("Status code " + statusCode);
+
+		result  +=  responseEntity.getBody();
+		System.out.println("response body " + result);
+
+		HttpHeaders responseHeaders = responseEntity.getHeaders();
+		System.out.println("response Headers " + responseHeaders);
+	}
+
+	public static void updateUserByExchangeMethod(HttpEntity<Object> requestEntity){
+		ResponseEntity<String> responseEntity = restTemplate.exchange(
+				baseUrl,
+				HttpMethod.PUT,
+				requestEntity,
+				String.class);
+		HttpStatus statusCode = responseEntity.getStatusCode();
+		System.out.println("Status code " + statusCode);
+
+		result  +=  responseEntity.getBody();
+		System.out.println("response body " + result);
+
+		HttpHeaders responseHeaders = responseEntity.getHeaders();
+		System.out.println("response Headers " + responseHeaders);
+	}
+
+	public static void deleteUserByExchangeMethod(HttpEntity<Object> requestEntity){
+		ResponseEntity<String> responseEntity = restTemplate.exchange(
+				baseUrl + "/3",
+				HttpMethod.DELETE,
+				requestEntity,
+				String.class);
+		HttpStatus statusCode = responseEntity.getStatusCode();
+		System.out.println("Status code " + statusCode);
+
+		result  +=  responseEntity.getBody();
+		System.out.println("response body " + result);
+
+		HttpHeaders responseHeaders = responseEntity.getHeaders();
+		System.out.println("response Headers " + responseHeaders);
 	}
 
 }
--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -1,72 +1,19 @@
 package ru.kata.spring.boot_security.demo.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
-import java.util.Collection;
-import java.util.Objects;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-@Entity
-@Table(name="users")
-public class User implements UserDetails {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+public class User {
     private Long id;
-    @Column(name="Username")
-    private String userName;
-    @Column(name="Lastname")
+    private String name;
     private String lastName;
-    @Column(name="Password")
-    private String password;
-    @Column(name="Age")
-    private int age;
-
-    @ManyToMany(fetch = FetchType.LAZY)
-    @JoinColumn(referencedColumnName = "user_id")
-    private Set<Role> roles;
+    private Byte age;
 
     public User() {
     }
 
-    public User(String userName, String lastName, String password, int age, Set<Role> roles) {
-        this.userName = userName;
+    public User(Long id, String name, String lastName, Byte age) {
+        this.id = id;
+        this.name = name;
         this.lastName = lastName;
-        this.password = password;
         this.age = age;
-        this.roles = roles;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Set<Role> getRoles() {
-        return this.roles;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
     }
 
     public Long getId() {
@@ -77,86 +24,28 @@ public class User implements UserDetails {
         this.id = id;
     }
 
-    public String getUserName() {
-        return userName;
+    public String getName() {
+        return name;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public String getLastName() {
+        return lastName;
     }
 
-    public int getAge() {
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Byte getAge() {
         return age;
     }
 
-    public void setAge(int age) {
+    public void setAge(Byte age) {
         this.age = age;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-//        Set<Role> roles = getRoles();
-        return getRoles();
-//        return roles.stream().map(r -> new SimpleGrantedAuthority(r.getRole())).collect(Collectors.toList());
-    }
-
-    @Override
-    public String getPassword() {
-        return this.password;
-    }
-
-    @Override
-    public String getUsername() {
-        return this.userName;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof User)) return false;
-        User user = (User) o;
-        return getAge() == user.getAge() && getId().equals(user.getId()) && getUserName().equals(user.getUserName()) && getLastName().equals(user.getLastName()) && getPassword().equals(user.getPassword()) && getRoles().equals(user.getRoles());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getUserName(), getLastName(), getPassword(), getAge(), getRoles());
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", userName='" + userName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", password='" + password + '\'' +
-                ", age=" + age +
-                ", roles=" + roles +
-                '}';
     }
 }
 
